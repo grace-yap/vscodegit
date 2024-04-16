ea126d9fafb1ca65be67fcdf3671e3ffea75691e
grace-yap <95622641+grace-yap@users.noreply.github.com>
2024-04-16 13:51:10 +0800

    Update script.js

--- build/js/script.js
+++ build/js/script.js
@@ -5,7 +5,8 @@ $(function () {
   let meetMonsters = document.querySelector('#meet')
   let monsterScroll = document.querySelectorAll('#monster-group .monster')
   let navHeight = nav.scrollHeight
-
+// TODO: Update these names to make them clearer.
+  
   monsterScroll.forEach(
     (item) => (item.style.animationDelay = `${Math.random() * 1 + 0.4}s`)
   )
